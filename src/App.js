1b7813f855826f75064f1d7ff296b928984a1516
mgamamunbd <mgamamunbd@gmail.com>
2022-03-18 13:05:28 +0400

    some updated

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
 function LoadCountries() {
   const [countries, setCountries] = useState([]);
-  // console.log(countries);
+  console.log(countries[0]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((response) => response.json())
@@ -24,8 +24,19 @@ function LoadCountries() {
       <h1>Countries</h1>
       <h3>Available Countries: {countries.length}</h3>
       {countries.map((country) => (
-        <p>Country: {country.name.common}</p>
+        <Country
+          name={country.name.common}
+          population={country.population}></Country>
       ))}
+    </div>
+  );
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h2>Population: {props.population}</h2>
     </div>
   );
 }
